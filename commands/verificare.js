58b32658d98758953a29eb9008a6ab5b5a36aaf0
HaZZe <32200147+HaZZeAdv@users.noreply.github.com>
2019-07-11 08:00:15 +0300

    Update verificare.js

--- commands/verificare.js
+++ commands/verificare.js
@@ -37,7 +37,7 @@ module.exports.run = async (bot, message, args) => {
           "este acum un Ucenic."
         ];
         let WelcomeMessage = Welcome[Math.floor(Math.random()*Welcome.length)];
-        member.guild.channels.find("name", "discuţii").send(member.toString() + " " + WelcomeMessage);
+        member.guild.channels.find("name", "📝chat").send(member.toString() + " " + WelcomeMessage);
 
       } else return;
   })
